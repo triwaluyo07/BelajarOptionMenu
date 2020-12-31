218cf729f3152cb57ede905b385ae86381db2d70
triwaluyo07 <triwaluyo432@gmail.com>
2020-12-31 18:41:28 +0700

    BelajarOptionMenuAndroid

---
+++ MyOptionMenu/app/src/main/java/triwaluyo/dicoding/myoptionmenu/SettingActivity.java
@@ -0,0 +1,20 @@
+package triwaluyo.dicoding.myoptionmenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+
+
+}
